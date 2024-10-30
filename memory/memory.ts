e2cfed98227d99add13d5808dcc0169300611b4f
Tal Ater <tal@talater.com>
2024-10-29 19:56:34 -0600

    Refactor Program Counter to use 16-bit representation

--- memory/memory.ts
+++ memory/memory.ts
@@ -3,7 +3,8 @@ const memory: Uint8Array = new Uint8Array(memoryBuffer);
 const ROM_START = 0x200;
 const FONT_START = 0x050;
 
-let PC: number = ROM_START; // The Program Counter. Points at the current instruction in memory
+const PC = new Uint16Array(1); // The Program Counter. Points at the current instruction in memory
+PC[0] = ROM_START;
 
 const storeFont = (data: number[]): boolean => {
   memory.set(data, FONT_START);
@@ -20,16 +21,16 @@ const read = (address: number): number => {
 };
 
 const fetch = (): number => {
-  const opcodes = (memory[PC] << 8) | memory[PC + 1];
-  PC += 2;
+  const opcodes = (memory[PC[0]] << 8) | memory[PC[0] + 1];
+  PC[0] += 2 & 0xFFFF; // Ensure PC stays 16-bit after increment
   return opcodes;
 };
 
 const setPC = (addr: number): void => {
-  PC = addr;
+  PC[0] = addr & 0xFFFF;
 };
 
-const getPC = (): number => PC;
+const getPC = (): number => PC[0];
 
 export { FONT_START, ROM_START };
 export { fetch, getPC, read, setPC, storeFont, storeROM };
