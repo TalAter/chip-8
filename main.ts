7898c4246ac63f077424fcfb93f319a41105bd98
Tal Ater <tal@talater.com>
2024-11-03 01:04:39 -0700

    `main.ts` now gets the rom file as a command line argument

--- main.ts
+++ main.ts
@@ -1,5 +1,12 @@
 import { init, mainLoop } from "./emulator/emulator.ts";
 
-init("./roms/IBMLogo.ch8");
+// Get command line argument
+const romFile = Deno.args[0];
 
+if (!romFile) {
+    console.error("Please provide a ROM file path as an argument");
+    Deno.exit(1);
+}
+
+init(romFile);
 mainLoop();
