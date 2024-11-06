f31d744d2c48cc9910efafb8917d6da0a2ae5ad8
Tal Ater <tal@talater.com>
2024-11-06 00:53:12 -0700

    Introduce `FONT_BYTES_PER_CHAR` constant

--- fonts/font.ts
+++ fonts/font.ts
@@ -1,4 +1,6 @@
-// deno-fmt-ignore-file
+export const FONT_BYTES_PER_CHAR = 5;
+
+// deno-fmt-ignore-line
 export const font: number[] = [
     0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
     0x20, 0x60, 0x20, 0x20, 0x70, // 1
